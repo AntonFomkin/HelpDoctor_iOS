02605ff01306f299133e9c17d36e0b9c48ac40c3
Anton Fomkin <ib2006a@yandex.ru>
2019-10-31 11:20:42 +0300

    Проведен рефакторинг кода Созданы новые методы для работы с API (до 11 включительно) В конце модуля NetworkService примеры вызова методов в UI

---
+++ HelpDoctor/GetListOfInterests.swift
@@ -0,0 +1,9 @@
+//
+//  GetListOfInterests.swift
+//  HelpDoctor
+//
+//  Created by Anton Fomkin on 28.10.2019.
+//  Copyright © 2019 Anton Fomkin. All rights reserved.
+//
+
+import Foundation
---
+++ HelpDoctor/MedicalSpecialization.swift
@@ -0,0 +1,9 @@
+//
+//  MedicalSpecialization.swift
+//  HelpDoctor
+//
+//  Created by Anton Fomkin on 23.10.2019.
+//  Copyright © 2019 Anton Fomkin. All rights reserved.
+//
+
+import Foundation
